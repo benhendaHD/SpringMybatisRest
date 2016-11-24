5e3e972a458baa2563e18701dc0a2d783970057a
benhenda89 <hafedhbenhenda@gmail.com>
2016-11-24 21:41:53 +0100

    Demo application Spring Mybatis Rest

--- src/main/java/com/hafedh/SpringRestMyBatisDemoApplication.java
+++ src/main/java/com/hafedh/SpringRestMyBatisDemoApplication.java
@@ -1,13 +1,9 @@
 package com.hafedh;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@MapperScan("com.hafedh.mappers")
-@Configuration
 public class SpringRestMyBatisDemoApplication {
 
     public static void main(final String[] args) {
